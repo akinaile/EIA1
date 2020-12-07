e97cb92c659eabaec08af1163fb29e70a0a6cd26
akinaile <aileen-akin@web.de>
2020-12-07 21:19:29 +0100

    Aufgabe07

---
+++ Steckbrief/Aufgabe07/script07.ts
@@ -0,0 +1,58 @@
+//Arrays
+const pads: HTMLAudioElement [] = []
+pads[0] = new Audio ("hihat.mp3");
+pads[1] = new Audio ("kick.mp3");
+pads[2] = new Audio ("snare.mp3");
+pads[3] = new Audio ("A.mp3");
+pads[4] = new Audio ("C.mp3");
+pads[5] = new Audio ("F.mp3");
+pads[6] = new Audio ("G.mp3");
+pads[7] = new Audio ("laugh-1.mp3");
+pads[8] = new Audio ("laugh-2.mp3");
+
+
+//Sounds abspielen
+function playSample(x:number){
+    pads[x].play;
+}
+
+//Sounds
+document.querySelector(".pad0").addEventListener("click", function(){
+    playSample(0);
+});
+document.querySelector(".pad1").addEventListener("click", function(){
+    playSample(1);
+});
+document.querySelector(".pad2").addEventListener("click", function(){
+    playSample(2);
+});
+document.querySelector(".pad3").addEventListener("click", function(){
+    playSample(3);
+});
+document.querySelector(".pad4").addEventListener("click", function(){
+    playSample(4);
+});
+document.querySelector(".pad5").addEventListener("click", function(){
+    playSample(5);
+});
+document.querySelector(".pad6").addEventListener("click", function(){
+    playSample(6);
+});
+document.querySelector(".pad7").addEventListener("click", function(){
+    playSample(7);
+});
+document.querySelector(".pad8").addEventListener("click", function(){
+    playSample(8);
+});
+
+//Button
+function interval() {
+    setInterval(function () {
+        pads[6].play();
+        pads[5].play();
+        pads[4].play();
+    }, 400);
+}
+document.querySelector(".button").addEventListener("click", function () {
+    interval();
+});
